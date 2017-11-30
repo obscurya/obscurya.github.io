05c01dcc4cc295fed3b4c197f681ef6be2e795fd
obscurya <prostunting@mail.ru>
2017-11-30 20:28:41 +0300

    newnewnew

--- void/particles/letters.js
+++ void/particles/letters.js
@@ -1,43 +1,50 @@
 var canvas = document.getElementById('canvas'),
     c = canvas.getContext('2d');
 
-canvas.width = 640;
+canvas.width = 800;
 canvas.height = 480;
 
-var string = 'МАКСИМКА';
+var string = 'obscurya';
 
-clear('#fff');
+clear('#000');
 
 function Char() {
-    var pixel = pixels[random(0, pixels.length - 1)];
+    var i = random(0, pixels.length - 1),
+        pixel = pixels[i];
 
     this.x = pixel.x;
     this.y = pixel.y;
 
     while (pixel.free === false) {
-        pixel = pixels[random(0, pixels.length - 1)];
+        i = random(0, pixels.length - 1);
+        pixel = pixels[i];
         this.x = pixel.x;
         this.y = pixel.y;
     }
 
     pixel.free = false;
 
+    if (pixels[i - 1]) { pixels[i - 1].free = false; }
+    if (pixels[i - 2]) { pixels[i - 2].free = false; }
+    if (pixels[i + 1]) { pixels[i + 1].free = false; }
+    if (pixels[i + 2]) { pixels[i + 2].free = false; }
+
     this.content = string.charAt(random(0, string.length - 1));
-    this.color = color(0, 0, 0, random(55, 255));
-    this.size = random(5, 15);
+    this.color = color(255, 255, 255, random(55, 155));
+    this.size = random(10, 20);
 }
 
 var data = [],
     pixels = [];
 
 function init(str) {
-    clear('#fff');
+    clear('#000');
 
     begin();
-    c.fillStyle = '#000';
+    c.fillStyle = '#fff';
     c.textAlign = 'center';
     c.textBaseline = 'middle';
-    c.font = '700 ' + canvas.width / str.length + 'px sans-serif';
+    c.font = '700 ' + canvas.width / str.length * 1.5 + 'px sans-serif';
     c.fillText(str, canvas.width / 2, canvas.height / 2);
     close();
 
@@ -51,7 +58,7 @@ function init(str) {
                 g = data[i + 1],
                 b = data[i + 2];
 
-            if (r == 0 && g == 0 && b == 0) {
+            if (r == 255 && g == 255 && b == 255) {
                 var pixel = {};
 
                 pixel.x = w;
@@ -68,7 +75,7 @@ function init(str) {
 
 function drawChar(char) {
     begin();
-    c.font = char.size + 'px sans-serif';
+    c.font = 'normal ' + char.size + 'px sans-serif';
     c.fillStyle = char.color;
     c.textAlign = 'center';
     c.textBaseline = 'middle';
@@ -82,8 +89,8 @@ var counter = 0,
     chars = [];
 
 function draw() {
-    if (counter < string.length * 150) {
-        clear('#fff');
+    if (counter < string.length * 50) {
+        clear('#000');
 
         chars.push(new Char);
 
