0884e34e02b0cad3dbe838db9aff48714f737a6e
obscurya <prostunting@mail.ru>
2018-01-07 23:01:04 +0300

    game v 0.5.1

--- void/game/hud.js
+++ void/game/hud.js
@@ -1,13 +1,15 @@
 function Hud() {
     this.draw = function () {
         c.beginPath();
-        c.fillStyle = '#fff';
+        c.fillStyle = color(255, 255, 255, 255 / 2);
         c.font = '14px sans-serif';
         c.textAlign = 'left';
         c.fillText('x: ' + (player.x + Math.abs(background.x)) + ' y: ' + (player.y + Math.abs(background.y)), 20, 30);
         c.fillText('angle: ' + (player.angle * 180 / Math.PI).toFixed(1), 20, 50);
         c.fillText('fireballs: ' + flames.length, 20, 70);
+        c.fillText('controls: ' + player.controls, 20, 90);
         // c.fillText('beginCast: ' + player.beginCast, 20, 90);
+        c.fillText('aim: ' + player.aim, 20, 110);
         // c.fillText('endCast: ' + player.endCast, 20, 110);
         // c.fillText('casting: ' + player.casting, 20, 130);
         c.closePath();
