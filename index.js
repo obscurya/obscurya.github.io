327b73f251d8a5c6e6d20030c82baf6302393683
obscurya <prostunting@mail.ru>
2018-01-11 17:26:11 +0300

    ob

---
+++ index.js
@@ -0,0 +1,11 @@
+var hello = function () {
+    var recipient = ['friend', 'buddy', 'human', 'Mark'],
+        rnd = Math.floor(Math.random() * (recipient.length));
+    return 'Oh, hi, ' + recipient[rnd] + '.';
+}
+
+var output = document.getElementById('hello'),
+    code = document.getElementById('code');
+
+output.innerHTML = hello();
+code.innerHTML = hello;
