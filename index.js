30a807a5520887c8136e487e895afd79cda75f5e
obscurya <prostunting@mail.ru>
2018-01-15 23:06:44 +0300

    upd

--- index.js
+++ index.js
@@ -22,21 +22,66 @@ function loadJSON(file, callback) {
     request.send(null);
 }
 
+var colors = [
+    '#E91E63',
+    '#9C27B0',
+    '#673AB7',
+    '#3F51B5',
+    '#212121',
+    '#4CAF50',
+    '#FFEB3B'
+];
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+function randomColor() {
+    var red = random(0, 255),
+        green = random(0, 255),
+        blue = random(0, 255);
+    return 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+}
+
+function randomMonochromeColor(min, max) {
+    var color = random(min, max);
+    return 'rgb(' + color + ', ' + color + ', ' + color + ')';
+}
+
 function formArticle(article) {
-    var output = '<article><div class="content"><table><tr><td class="title">' + article.title + '</td>';
+    var output = '<article><div class="content"><p class="title">' + article.title + '</p><div class="buttons">';
+    if (article.img !== '') {
+        output += '<div class="img" style="background-image: url(images/' + article.img + ');"></div>';
+    } else {
+        var color1 = randomMonochromeColor(75, 205),
+            color2 = randomMonochromeColor(75, 205);
+        output += '<div class="img" style="background-image: linear-gradient(45deg, ' + color1 + ', ' + color2 + ');"></div>';
+    }
     if (article.link_view !== '') {
-        output += '<td class="view"><a href="' + article.link_view + '" target="_blank"><i class="material-icons">visibility</i></a></td>';
+        output += '<div class="view"><a href="' + article.link_view + '"><i class="material-icons">visibility</i><br>Посмотреть</a></div>';
     }
     if (article.link_source !== '') {
-        output += '<td class="source"><a href="' + article.link_source + '" target="_blank"><i class="material-icons">code</i></a></td>';
+        output += '<div class="source"><a href="' + article.link_source + '"><i class="material-icons">code</i><br>Исходный код</a></div>';
     }
-    output += '</table><div class="body"><p class="desc" style="width: 100%; padding-right: 0px;">' + article.desc + '</p>';
-    if (article.img !== '') {
-        output += '<div class="img"><img src="images/' + article.img + '"></div>';
-    }
-    output += '</div></div></article>';
+    output += '</div><p class="desc">' + article.desc + '</p>';
     return output;
 }
+
+// function formArticle(article) {
+//     var output = '<article><div class="content"><table><tr><td class="title">' + article.title + '</td>';
+//     if (article.link_view !== '') {
+//         output += '<td class="view"><a href="' + article.link_view + '" target="_blank"><i class="material-icons">visibility</i></a></td>';
+//     }
+//     if (article.link_source !== '') {
+//         output += '<td class="source"><a href="' + article.link_source + '" target="_blank"><i class="material-icons">code</i></a></td>';
+//     }
+//     output += '</table><div class="body"><p class="desc" style="width: 100%; padding-right: 0px;">' + article.desc + '</p>';
+//     if (article.img !== '') {
+//         output += '<div class="img"><img src="images/' + article.img + '"></div>';
+//     }
+//     output += '</div></div></article>';
+//     return output;
+// }
 
 function formEmpty() {
     var output = '<article><div class="content"><p>Тут пусто.</p></div></article>';
