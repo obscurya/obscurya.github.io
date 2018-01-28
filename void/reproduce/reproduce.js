18e3f35ac2c071e11295c5e708e6d842de82b986
obscurya <prostunting@mail.ru>
2018-01-28 09:36:31 +0300

    upd

--- void/reproduce/reproduce.js
+++ void/reproduce/reproduce.js
@@ -315,11 +315,11 @@ function start() {
     if (file) {
         reader.readAsDataURL(file);
     } else {
-        img.src = 'girl.jpg';
+        // img.src = 'girl.jpg';
     }
 }
 
-start();
+// start();
 
 function save() {
     if (polygons) {
