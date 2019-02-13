d51302816aaae4e63a0a405ad1c658d6876ed3a4
obscurya <prostunting@mail.ru>
2019-02-13 21:12:38 +0300

    commit

---
+++ precache-manifest.6b7c13a4dc9c4dc9e3abf00a706a0229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d25e70594455b4e4259b",
+    "url": "/static/js/main.d25e7059.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "d25e70594455b4e4259b",
+    "url": "/static/css/main.e33bc43e.chunk.css"
+  },
+  {
+    "revision": "c4ec6518ef7a3b61f86975e977cb894e",
+    "url": "/index.html"
+  }
+];
